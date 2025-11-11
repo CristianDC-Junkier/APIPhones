814ed84c6b58a623d09484f69d93f218cf69b1cc
informaticaalmonte <informaticaalcaldia@aytoalmonte.es>
2025-11-11 11:17:46 +0100

    notifications fix

--- backend/controllers/MailerController.js
+++ backend/controllers/MailerController.js
@@ -1,6 +1,6 @@
 ﻿const nodemailer = require("nodemailer");
 
-const { UserAccount } = require("../models/Relations");
+const { UserAccount, Department } = require("../models/Relations");
 
 const LoggerController = require("./LoggerController");
 const { Op } = require("sequelize");
@@ -28,8 +28,9 @@ class MailerController {
 
     /**
      * Mandar notificación de Ticket nuevo a las cuentas Admin y SuperAdmin que tengan correo
+     * @param {Object} ticket - Objeto con la información del ticket sobre el que se quiere avisar
      */
-    static async sendNotif() {
+    static async sendNotif(ticket) {
         try {
             //Verifica que el transporter se puede conectar
             await transporter.verify();
@@ -38,16 +39,33 @@ class MailerController {
             const users = await UserAccount.findAll({
                 where: { mail: { [Op.ne]: null } }
             });
-            const formatted = users.map(user => (
+            const mails = users.map(user => (
                 user.mail
             ));
 
+            const ticketU = await UserAccount.findByPk(ticket.userRequesterId);
+            const depU = await Department.findByPk(ticketU.departmentId);
+
+            const reminderStyle = 'color:gray; margin-top:20px; font-size:0.75rem;';
+
+            const htmlMailBody = `
+            <div>
+                <div>Un nuevo ticket ha sido generado en la página del Listín Telefónico del Ayuntamiento de Almonte a fecha: ${ticket.createdAt} por el usuario del departamento <strong>${depU.name}</strong>.</div>
+                <div> 
+                    <strong>Asunto:&nbsp;</strong>${ticket.topic}
+                </div>
+                <div style="${reminderStyle}">
+                    Este mensaje ha sido enviado de forma automática y cualquier respuesta al mismo será ignorada. <br>
+                    En caso de recibir esta notificación sin su consentimiento, por favor, pongase en contacto con informaticaalcaldia@aytoalmonte.es.
+                </div>
+            </div>`;
+
             //Manda la notificación
             const notification = await transporter.sendMail({
-                from: 'informaticaalcaldia@aytoalmonte.es', // Dirección del emisor
-                to: formatted.join(", "), //Lista de los receptores
+                from: 'No Reply <informaticaalcaldia@aytoalmonte.es>', // Dirección del emisor
+                to: mails.join(", "), //Lista de los receptores
                 subject: "Nuevo Ticket en Listin Telefonico", //Asunto
-                html: "Un nuevo ticket ha sido generado en la página del Listín Telefónico del ayuntamiento de Almonte.", //Cuerpo del mensaje en HTML
+                html: htmlMailBody, //Cuerpo del mensaje en HTML
                 auth: { //Credenciales
                     user: process.env.SMTP_USER,
                     refreshToken: process.env.SMTP_REFRESHTOKEN,
